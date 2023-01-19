3b7aaeaef6caa2323bfd705a9c5ca86d60f5d59b
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2023-01-19 03:09:56 -0300

    Modificaciones en el Front App v0.18

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,27 +16,54 @@ export default function Home() {
   let MATTIOLI = TITULARES[0]
   let loding = false
 
-  const {parcelaHome}=useParcelasHome()
-  
+  const { parcelaHome } = useParcelasHome()
+
+  let data = parcelaHome.map(parcela => parcela.nom_cat.mz.toLowerCase().replace(' ', ''));
+  const dataMz = new Set(data);
+  let manzanas = [...dataMz];
+
   return (
     <section className='contenedor-stadar'>
-      <article className='contenedor-lista-terrenos'>
-        <h2>Informacion general de los terrenos</h2>
-        <div>
-          {loding
-            ? <p>CARGANDO ... </p>
-            : <>
-              <h3>{`${MATTIOLI.titular} (${MATTIOLI.parcelas.length})`}</h3>
-              {parcelaHome.map((parcela, i) => {
-                return <p key={`parcela-${i}`} > 
-                          <b className={`parcela-${parcela.titularidad}`}>{`${parcela.partida} - `}</b>{`MZ:${parcela.nom_cat.mz} PA:${parcela.nom_cat.pa}`}
-                        </p>
-              })}
+      
+      <h2>Informacion general de los terrenos</h2>
+      <h3>{`${MATTIOLI.titular} (${MATTIOLI.parcelas.length})`}</h3>
+      <div className='contenedor-home'>
+          <article className='contenedor-lista-terrenos'>
+            <div className='contenedor'>
+              {loding
+                ? <p>CARGANDO ... </p>
+                : <>
+                  <h3><u>PARCELAS</u></h3>
+                  {parcelaHome.map((parcela, i) => {
+                    return <p key={`parcela-${i}`} >
+                      <b className={`parcela-${parcela.titularidad}`}>{`${parcela.partida}`}</b>
+                     
+                      <b> - MZ: </b>{`${parcela.nom_cat.mz}`}
+                      &nbsp; &nbsp;
+                      <b>PA: </b>{`${parcela.nom_cat.pa}`}
+                    </p>
+                  })}
 
-            </>
-          }
+                </>
+              }
+            </div>
+          </article>
+
+          <div className='contenedor-mapas'>
+          <article className='contenedor-imagen-mz-dx'>
+            <img src={`/Proyecto-EstadoParcelario/imagenes-manzanas/dx-mz.png`} alt={`parcelas-dx-mz.png`} />
+          </article>
+          <div className='contenedor-imagen-mzs'>
+            {manzanas.map((manzana, i) => {
+              return <article key={`manzana-${i}`} className='contenedor-imagen-mz'>
+                <img src={`/Proyecto-EstadoParcelario/imagenes-manzanas/mz${manzana}.png`} alt={`mz-${manzana}.png`} />
+              </article>
+            })}
+          </div>
         </div>
-      </article>
+      </div>
+
+        
       
     </section>
   )
